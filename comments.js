d035a462576ea3e4102739149ab598eb61753a5a
GISELLEQASCM <gipachecof@gmail.com>
2025-03-25 13:59:08 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,36 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. Use the comments.html file from the previous exercise.
+//
+// Create a new file named comments.js.
+// In comments.js, create a web server that listens on port 3000 and serves the comments.html file.
+// Use the createServer method from the http module.
+// Use the fs module to read the comments.html file.
+// Use the response object to serve the comments.html file.
+// Export the server using the module.exports syntax.
+// In the main file, require the comments.js file.
+// Use the server object from the comments.js file to start the server.
+// Run the main file using the node command.
+// Visit http://localhost:3000 in your web browser to see the comments.html file.
+// Use the comments.html file from the previous exercise.
+// Use the createServer method from the http module.
+// Use the fs module to read the comments.html file.
+// Use the response object to serve the comments.html file.
+// Export the server using the module.exports syntax.
+// In the main file, require the comments.js file.
+// Use the server object from the comments.js file to start the server.
+// Run the main file using the node command.
+// Visit http://localhost:3000 in your web browser to see the comments.html file.
+// Use the comments.html file from the previous exercise.
+// Use the createServer method from the http module.                                                      
+// Use the fs module to read the comments.html file.
+// Use the response object to serve the comments.html file.
+// Export the server using the module.exports syntax.
+// In the main file, require the comments.js file.
+// Use the server object from the comments.js file to start the server.
+// Run the main file using the node command.
+// Visit http://localhost:3000 in your web browser to see the comments.html file.
+// Use the comments.html file from the previous exercise.
+// Use the createServer method from the http module.
+// Use the fs module to read the comments.html file.
+// Use the response object to serve the comments.html file.
+// Export the server using the module.exports syntax.                              
